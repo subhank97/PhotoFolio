672e304dc4c1275b7dee88d1dd4835c492f0c8b8
Subhan Khan <subhank1997@gmail.com>
2023-04-06 15:56:52 -0400

    debugging

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-require('dotenv').config()
+// require('dotenv').config()
 
 ReactDOM.render(
   <React.StrictMode>
