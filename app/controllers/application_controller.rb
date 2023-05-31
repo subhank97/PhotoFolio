9d57e07bec5f456aa45e63f699300ef0df68d6fd
Subhan Khan <subhank1997@gmail.com>
2023-05-31 12:49:37 -0400

    updated routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
-  before_action :authorize
+  before_action :authorize, except: :new
+
 
   private
 
