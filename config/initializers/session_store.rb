cf8b9893a1d44b9b5616b40a26ca59b5be16c309
Subhan Khan <subhank1997@gmail.com>
2023-06-04 19:41:55 -0400

    updating application.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', secure: true
+Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', secure: Rails.env.production?
