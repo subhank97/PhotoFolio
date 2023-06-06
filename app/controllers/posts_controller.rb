d4ceb78c18387da5cb862ce5d1d44172bba6e183
Subhan Khan <subhank1997@gmail.com>
2023-06-06 17:54:14 -0400

    updated controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authorize, only: [:create]
+  # before_action :authorize, only: [:create]
 
   def index
     user = User.find(params[:user_id])
