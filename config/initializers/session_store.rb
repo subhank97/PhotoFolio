e6c89c661da1b355b8e77e0a9911cee6fe16d92e
Subhan Khan <subhank1997@gmail.com>
2023-06-12 19:41:25 -0400

    debug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', domain: :all, tld_length: 2, same_site: :none, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', domain: :all, tld_length: 2, same_site: :lax, secure: Rails.env.production?
