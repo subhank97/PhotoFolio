365539f242d04ab6be47e607a993d3318587fcd6
Subhan Khan <subhank1997@gmail.com>
2023-06-12 17:31:21 -0400

    updating

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Allow all origins (temporarily)
+    origins 'https://pictogram-b63u.onrender.com'
 
     resource '*',
       headers: :any,
