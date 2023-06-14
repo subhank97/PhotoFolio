0ea839d66d98e97cc79728bf06f8d81f41504353
Subhan Khan <subhank1997@gmail.com>
2023-06-14 18:30:12 -0400

    dealing with 401 error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://not-instagram-backend.onrender.com' }
 
-  config.action_controller.default_url_options = { host: 'https://not-instagram-backend.onrender.com' }
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
