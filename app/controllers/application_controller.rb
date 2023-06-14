0120d0ebdce22fe6ab8107901986352616b65e32
Subhan Khan <subhank1997@gmail.com>
2023-06-14 17:35:01 -0400

    dealing with 401 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,5 +36,6 @@ class ApplicationController < ActionController::API
     Rails.logger.debug "Session data: #{session.to_hash}"
     @current_user ||= User.find_by(id: session[:user_id])
     Rails.logger.debug "Current user: #{@current_user}"
+    Rails.logger.debug "Current user ID: #{@current_user&.id}"
   end
 end
