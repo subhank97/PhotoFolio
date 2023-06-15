f3f1dcea26133926ce04e0d6e52f4986991b8d95
Subhan Khan <subhank1997@gmail.com>
2023-06-15 17:42:56 -0400

    post data is sending user data. Debugging

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,11 +12,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 end
-
-
-
-
-
-
-
-
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,11 @@
-Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', same_site: :none, secure: true
+if Rails.env.production?
+    Rails.application.config.session_store :cookie_store, 
+                                           key: '_not_instagram_session', 
+                                           domain: :all, 
+                                           same_site: :none, 
+                                           secure: true, 
+                                           tld_length: 3
+  else
+    Rails.application.config.session_store :cookie_store, 
+                                           key: '_not_instagram_session'
+  end
