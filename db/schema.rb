32862b13e2f52286515de7019905a97f8f4a9f54
Subhan Khan <subhank1997@gmail.com>
2023-06-15 10:06:22 -0400

    updating database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_135600) do
+ActiveRecord::Schema.define(version: 2023_06_15_140202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
