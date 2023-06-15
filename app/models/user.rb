936df5c211e07fb305fdfa0bb9504a9646497ae8
Subhan Khan <subhank1997@gmail.com>
2023-06-15 10:40:43 -0400

    updating database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable, email: false
+       :recoverable, :rememberable, :trackable, :validatable,
+       email: false
 
   attr_accessor :email
 
