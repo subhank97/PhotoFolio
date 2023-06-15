3462c8687eb7b7e34c9097d6c6ac5fe91c28b24f
Subhan Khan <subhank1997@gmail.com>
2023-06-15 11:53:24 -0400

    configuring devise

--- db/migrate/20230615135600_clear_users.rb
+++
@@ -1,9 +0,0 @@
-class ClearUsers < ActiveRecord::Migration[6.1]
-  def up
-    User.destroy_all
-  end
-
-  def down
-    # No rollback needed
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_142544) do
+ActiveRecord::Schema.define(version: 2023_06_15_023901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,14 +61,15 @@ ActiveRecord::Schema.define(version: 2023_06_15_142544) do
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
-    t.string "username", unique: true
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
