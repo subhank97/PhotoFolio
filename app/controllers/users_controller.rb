17ead3054442dc227484a25186f3a85897741975
Subhan Khan <subhank1997@gmail.com>
2023-06-15 12:03:34 -0400

    configuring devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create, :current]
+  before_action :authenticate_user!, except: [:create, :current]
 
   def index
     Rails.logger.debug "Fetching all users"
