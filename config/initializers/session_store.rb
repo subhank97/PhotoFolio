fca8d0aa7d9c3171fe9067d52ad2e8a6d706a7ea
Subhan Khan <subhank1997@gmail.com>
2023-06-15 17:29:54 -0400

    post data is sending user data. Debugging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', domain: :all, tld_length: 2, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_not_instagram_session', same_site: :none, secure: true
