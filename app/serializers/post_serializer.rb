90856530ef403c3df016f70543736c3b2725d9a9
Subhan Khan <subhank1997@gmail.com>
2023-06-17 18:49:50 -0400

    post fetch is sending back user data. Looking for issue

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :description, :user_id, :image_url, :user
+  attributes :id, :description, :user_id, :image_url, 
 
-  def user
-    object.user.as_json(only: [:id, :full_name])
-  end
 end
