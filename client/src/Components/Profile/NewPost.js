fe1a6a3eeae9da6c91e9841db36f960edfc82904
Subhan Khan <subhank1997@gmail.com>
2023-10-13 19:03:01 -0400

    updated form to create a new post on profile page

--- client/src/Components/Profile/NewPost.js
+++ client/src/Components/Profile/NewPost.js
@@ -7,12 +7,16 @@ function NewPost({ user, setProfilePosts }) {
 
   const [imageFile, setImageFile] = useState(null);
   const [description, setDescription] = useState('');
+  const [isImageSelected, setIsImageSelected] = useState(false);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setImageFile(file);
     if (file) {
-      toast.success("Image selected");
+      toast.success('Image selected');
+      setIsImageSelected(true);
+    } else {
+      setIsImageSelected(false);
     }
   };
 
@@ -39,15 +43,16 @@ function NewPost({ user, setProfilePosts }) {
           }
         })
         .then((newPost) => {
-          console.log('New post:', newPost);
+          //console.log('New post:', newPost);
           setProfilePosts((prevPosts) => {
             if (Array.isArray(prevPosts)) {
               return [newPost, ...prevPosts];
             } else {
-              console.error('prevPosts is not an array:', prevPosts);
+              //console.error('prevPosts is not an array:', prevPosts);
               return [newPost];
             }
           });
+          setIsImageSelected(false)
           setImageFile(null);
           setDescription('');
         })
@@ -65,22 +70,38 @@ function NewPost({ user, setProfilePosts }) {
 
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-sm">
-      <div className="flex items-center border-b border-amber-400 py-2">
-        <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Caption" value={description} onChange={(e) => setDescription(e.target.value)} />
+      <div className="flex items-center border-amber-400 h-14 px-1">
+        <input
+          className="appearance-none bg-transparent border-none w-full text-white mr-3 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="Caption"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <div>
-          <input id="image-upload" type="file" accept="image/*" onChange={handleFileChange} style={{ display: 'none' }} />
-          <label htmlFor="image-upload" className="flex-shrink-0 border-transparent border-4 text-amber-400 hover:text-amber-600 text-sm py-1 px-2 rounded cursor-pointer">
-            <p>Select image</p>
+          <input
+            id="image-upload"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+          />
+          <label
+            htmlFor="image-upload"
+            className="flex-shrink-0 border-transparent border-4 text-amber-400 hover:text-amber-600 text-sm py-1 px-2 rounded cursor-pointer"
+          >
+            <p>{!isImageSelected ? "Select image" : ""}</p>
           </label>
         </div>
-        <button className="flex-shrink-0 bg-amber-400 hover:bg-amber-600 border-amber-400 hover:border-amber-600 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+        <button
+          className={`flex-shrink-0 text-md py-1 px-5 rounded ${isImageSelected ? 'bg-amber-600 border-amber-600' : 'bg-gray-400 opacity-25 cursor-default'
+            } text-white hover:border-amber-600`}
+          type="submit"
+        >
           Post
         </button>
       </div>
-      <ToastContainer
-        className="toast-container"
-        toastClassName="bg-yellow-400 text-black"
-      />
+      <ToastContainer className="toast-container" toastClassName="bg-yellow-400 text-black" />
     </form>
   );
 }
